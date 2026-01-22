2471e9516241cec673f201835d0bfba735c1b350
Raúl Adamuz Cano <radamuzc@gmail.com>
2026-01-22 15:40:50 +0100

    feat: enhance run.sh to include logging and validate AWS credentials; ensure logs directory exists

--- run.sh
+++ run.sh
@@ -14,7 +14,10 @@ done
 # Fin Elegir un Dockerfile
 
 # Arrancar proceso de construcción de imágenes Docker
-bash scripts/docker-build.sh "$DOCKERFILE_PATH"
+RUN_DOCKER_BUILD=true
+if $RUN_DOCKER_BUILD; then
+  bash scripts/docker-build.sh "$DOCKERFILE_PATH"
+fi
 # Fin Arrancar proceso de construcción de imágenes Docker
 
 # Elegir un perfil de AWS 
@@ -46,10 +49,29 @@ done
 # Fin Elige una región de AWS
 
 # Test de credenciales
-if aws sts get-caller-identity --profile "$AWS_PROFILE" | jq; then
+AWS_STS_GET_CALLER_IDENTITY=$(aws sts get-caller-identity --profile "$AWS_PROFILE")
+AWS_STS_GET_CALLER_IDENTITY_STATUS=$?
+echo "$AWS_STS_GET_CALLER_IDENTITY" | jq
+if [ $AWS_STS_GET_CALLER_IDENTITY_STATUS -eq 0 ]; then
     echo "Las credenciales de AWS son válidas."
 else
     echo "Error: Las credenciales de AWS no son válidas."
     exit 1
 fi
 # Fin Test de credenciales
+
+# Asegurarse de que existe la carpeta logs
+mkdir -p logs
+# Fin Asegurarse de que existe la carpeta logs
+
+# Guardar registro de variables usadas
+LOG_FILE="logs/$(date '+%Y-%m-%d_%H-%M-%S').log"
+{
+  echo "LOG_FILE=$LOG_FILE"
+  echo "DOCKERFILE_PATH=$DOCKERFILE_PATH"
+  echo "AWS_PROFILE=$AWS_PROFILE"
+  echo "AWS_REGION=$AWS_REGION"
+  echo "AWS_STS_GET_CALLER_IDENTITY=$AWS_STS_GET_CALLER_IDENTITY"
+  echo "AWS_STS_GET_CALLER_IDENTITY_STATUS=$AWS_STS_GET_CALLER_IDENTITY_STATUS"
+} > "$LOG_FILE"
+# Fin Guardar registro de variables usadas
