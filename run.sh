0fb2a19060d2b599e0c5806d5908c09fe52a0e6b
Raúl Adamuz Cano <radamuzc@gmail.com>
2026-01-28 11:37:03 +0100

    feat: enhance run.sh with color-coded block messages for improved visibility during execution

--- run.sh
+++ run.sh
@@ -81,16 +81,21 @@ echo -e "${GREEN}Fin Bloque Test de credenciales${NC}"
 # Fin Test de credenciales
 
 # Introduce el nombre del aplicativo a desplegar
+echo -e "${CYAN}Inicio Bloque Introduce el nombre del aplicativo a desplegar${NC}"
 read -p "Nombre del aplicativo: " APP_NAME
 APP_NAME=${APP_NAME:-aws-ec2-docker-deployer}
 APP_NAME=${APP_NAME}-aedd
+echo -e "${GREEN}Fin Bloque Introduce el nombre del aplicativo a desplegar${NC}"
 # Fin Introduce el nombre del aplicativo a desplegar
 
 # Asegurarse de que existe la carpeta keypairs
+echo -e "${CYAN}Inicio Bloque Asegurarse de que existe la carpeta keypairs${NC}"
 mkdir -p keypairs
+echo -e "${GREEN}Fin Bloque Asegurarse de que existe la carpeta keypairs${NC}"
 # Fin Asegurarse de que existe la carpeta keypairs
 
 # Crear key pair
+echo -e "${CYAN}Inicio Bloque Crear key pair${NC}"
 CREATE_KEY_PAIR=true
 if $CREATE_KEY_PAIR; then
   if aws ec2 describe-key-pairs \
@@ -105,18 +110,22 @@ if $CREATE_KEY_PAIR; then
       --output text > "keypairs/$APP_NAME.$AWS_REGION.pem"
   fi
 fi
+echo -e "${GREEN}Fin Bloque Crear key pair${NC}"
 # Fin crear key pair
 
 # Crear security group
+echo -e "${CYAN}Inicio Bloque Crear security group${NC}"
 CREATE_SECURITY_GROUP=true
 if $CREATE_SECURITY_GROUP; then
 aws ec2 create-security-group \
   --group-name "$APP_NAME-sg" \
   --description "Security group for $APP_NAME"
 fi
+echo -e "${GREEN}Fin Bloque Crear security group${NC}"
 # Fin crear security group
 
 # Obtener el security group ID
+echo -e "${CYAN}Inicio Bloque Obtener el security group ID${NC}"
 if $CREATE_SECURITY_GROUP; then
 SECURITY_GROUP_ID=$(
   aws ec2 describe-security-groups \
@@ -124,9 +133,11 @@ SECURITY_GROUP_ID=$(
   --query 'SecurityGroups[0].GroupId' \
   --output text)
 fi
+echo -e "${GREEN}Fin Bloque Obtener el security group ID${NC}"
 # Fin Obtener el security group ID
 
 # Agregar reglas al security group
+echo -e "${CYAN}Inicio Bloque Agregar reglas al security group${NC}"
 if $CREATE_SECURITY_GROUP; then
 aws ec2 authorize-security-group-ingress \
   --group-id "$SECURITY_GROUP_ID" \
@@ -144,21 +155,29 @@ aws ec2 authorize-security-group-ingress \
   --port 443 \
   --cidr 0.0.0.0/0
 fi
+echo -e "${GREEN}Fin Bloque Agregar reglas al security group${NC}"
 # Fin agregar reglas al security group
 
 # Seleccionar VPC y Subnet
+echo -e "${CYAN}Inicio Bloque Seleccionar VPC y Subnet${NC}"
 source scripts/select-vpc-subnet.sh
+echo -e "${GREEN}Fin Bloque Seleccionar VPC y Subnet${NC}"
 # Fin Seleccionar VPC y Subnet
 
 # Obtener el AMI ID
+echo -e "${CYAN}Inicio Bloque Obtener el AMI ID${NC}"
 source scripts/get-ami-id-ubuntu.sh
+echo -e "${GREEN}Fin Bloque Obtener el AMI ID${NC}"
 # Fin Obtener el AMI ID
 
 # Comprobar si existe la EC2
+echo -e "${CYAN}Inicio Bloque Comprobar si existe la EC2${NC}"
 source scripts/check-ec2-name.sh
+echo -e "${GREEN}Fin Bloque Comprobar si existe la EC2${NC}"
 # Fin comprobar si existe la EC2
 
 # Si la EC2 existe entonces creala
+echo -e "${CYAN}Inicio Bloque Si la EC2 existe entonces creala${NC}"
 if $EC2_EXISTS; then
   echo "❌ La EC2 '$APP_NAME' ya existe."
 else
@@ -190,13 +209,17 @@ else
   echo "✅ PUBLIC_IP: $PUBLIC_IP"
   # Fin Obtener la dirección IP pública
 fi
+echo -e "${GREEN}Fin Bloque Si la EC2 existe entonces creala${NC}"
 # Fin Si la EC2 existe entonces creala
 
 # Asegurarse de que existe la carpeta logs
+echo -e "${CYAN}Inicio Bloque Asegurarse de que existe la carpeta logs${NC}"
 mkdir -p logs
+echo -e "${GREEN}Fin Bloque Asegurarse de que existe la carpeta logs${NC}"
 # Fin Asegurarse de que existe la carpeta logs
 
 # Guardar registro de variables usadas
+echo -e "${CYAN}Inicio Bloque Guardar registro de variables usadas${NC}"
 LOG_FILE="logs/$(date '+%Y-%m-%d_%H-%M-%S').log"
 {
   echo "LOG_FILE=$LOG_FILE"
@@ -210,4 +233,5 @@ LOG_FILE="logs/$(date '+%Y-%m-%d_%H-%M-%S').log"
   echo "EC2_RUN_INSTANCES_OUTPUT_JSON=$EC2_RUN_INSTANCES_OUTPUT_JSON"
   echo "INSTANCE_ID=$INSTANCE_ID"
 } > "$LOG_FILE"
+echo -e "${GREEN}Fin Bloque Guardar registro de variables usadas${NC}"
 # Fin Guardar registro de variables usadas
